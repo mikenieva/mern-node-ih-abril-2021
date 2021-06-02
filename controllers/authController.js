7bc02b5c02d2946e03a29c2c86e6897d67aba49f
Mike Nieva <m@mikenieva.com>
2021-06-02 12:27:40 -0500

    Autenticación terminada

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,17 +5,97 @@ const bcryptjs = require("bcryptjs")
 const { validationResult } = require("express-validator")
 const jwt = require("jsonwebtoken")
 
-
 // 2. CONTROLLERS
+
+// iniciarSesionUsuario
+// El objetivo es obtener el token, confirmando que el usuario y contraseña es correcto.
 exports.iniciarSesionUsuario = async(req, res) => {
 
+    // REVISAR SI HAY ERRORES EN LA VALIDACIÓN DE CHECKS
+    const errores = validationResult(req)
+    
+    if(!errores.isEmpty()){
+        return res.json({
+            errores: errores.array()
+        })
+    }
+
+    // EXTRAER EL EMAIL Y EL PASSWORD PARA VERIFICAR CONTRA BASE DE DATOS QUE SON LOS REALES
+
+    const { email, password  } = req.body
+
+    // REVISIÓN QUE EL USUARIO ESTÉ REGISTRADO EN BASE DE DATOS
+    try{
+        // ENCONTRAR AL USUARIO EN BD
+        let usuarioBD = await Usuario.findOne({ email })
+
+            // EN CASO DE QUE NO ENCONTREMOS AL USUARIO
+            if(!usuarioBD){
+                return res.status(400).json({
+                    msg: "El usuario no existe. Crea uno."
+                })
+            }
+
+        // REVISAR EL PASSWORD Y CONFIRMAR QUE ES EL PASSWORD CORRECTO
+        let passwordCorrecto = await bcryptjs.compare(password, usuarioBD.password)
+
+
+
+        if(!passwordCorrecto){
+            return res.status(400).json({
+                msg: "Password incorrecto. Intenta nuevamente."
+            })
+        }
+
+
+        // SI TRIUNFAMOS. TODO ESTÁ OK. ES EL USUARIO. SU PASS ESTÁ OK. CHÉVERE.
+
+        // CREAR EL PAYLOAD
+        const payload = {
+            usuario: {
+                id: usuarioBD.id
+            }
+        }
+
+
+
+        // CREAR EL JWT
+        jwt.sign(
+            payload,
+            process.env.SECRETA,
+            {
+                expiresIn: 360000 // EXPIRACIÓN 100 HORAS
+            },
+            (error, token) => {
+
+                if(error) throw error
+
+                // TRIUNFAMOS. DEVOLVAMOS EL TOKEN HACIA EL CLIENTE (POSTMAN O CHROME)
+                res.json({token})
+            }
+        )
+    } catch(e){
+        res.status(400).json({
+            msg: "Hubo un error en el servidor"
+        })
+    }
 }
 
-
 exports.verificarUsuario = async(req, res) => {
-        res.json({
-            usuario: req.usuario
-        })
+
+        const userId = req.usuario.id
+
+        try{
+            let usuario = await Usuario.findById(userId).select('-password')
+            res.json({
+                usuario
+            })
+
+        }catch(e){
+            res.status(400).json({
+                msg: "Hubo un error en el servidor."
+            })
+        }
 }
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ const auth = require('./../middleware/auth')
 */
 router.post("/", [
     check("email", "Agrega un email válido").isEmail(),
-    check("password", "El password debe ser mínimo de 6 carácteres").isLength({min: 6})
+    check("password", "El password debe ser mínimo de 6 caracteres").isLength({min: 6})
 ], authController.iniciarSesionUsuario)
 
 // VERIFICAR USUARIO
